4eca4ba2b31ffce932626213505224f24c6240fc
Sen <at437947@gmail.com>
2024-07-14 20:11:32 +0500

    feature: что то наваякал

--- src/pages/TestTransaction/TestTransactionPage.tsx
+++ src/pages/TestTransaction/TestTransactionPage.tsx
@@ -1,5 +1,29 @@
 import type { FC } from "react";
+import { SendTransactionRequest, useTonConnectUI } from "@tonconnect/ui-react";
+
+const transaction = {
+  messages: [
+    {
+      address:
+        "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
+      amount: "20000000", //Toncoin in nanotons
+    },
+  ],
+  //transaction body
+};
 
 export const TestTransactionPage: FC = () => {
-  return <h1>LDFLAGS</h1>;
+  const [tonConnectUI] = useTonConnectUI();
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          tonConnectUI.sendTransaction(transaction as SendTransactionRequest)
+        }
+      >
+        Send transaction
+      </button>
+    </div>
+  );
 };
