04fae4c8bb702b36f5c607850a9c83da0db9198f
Sen <at437947@gmail.com>
2024-07-14 23:29:18 +0500

    feature: что то наваякал

--- src/pages/TestTransaction/TestTransactionPage.tsx
+++ src/pages/TestTransaction/TestTransactionPage.tsx
@@ -1,27 +1,32 @@
 import type { FC } from "react";
-import { SendTransactionRequest, useTonConnectUI } from "@tonconnect/ui-react";
+import { useTonConnectUI } from "@tonconnect/ui-react";
+import { toNano } from "@ton/ton";
+import { beginCell } from "@ton/ton";
 
-const transaction = {
+const body = beginCell()
+  .storeUint(0, 32) // write 32 zero bits to indicate that a text comment will follow
+  .storeStringTail("Hello, TON!") // write our text comment
+  .endCell();
+
+// const [tonConnectUI] = useTonConnectUI();
+const myTransaction = {
+  validUntil: Math.floor(Date.now() / 1000) + 360,
   messages: [
     {
-      address:
-        "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
-      amount: "20000000", //Toncoin in nanotons
+      address: "UQDwfz1cu0nchgSl88_IQbJMnnFAC8jQkbt58ZPFErushP4b",
+      amount: toNano(0.05).toString(),
+      payload: body.toBoc().toString("base64"), // payload with comment in body
     },
   ],
-  //transaction body
 };
 
 export const TestTransactionPage: FC = () => {
-  const [tonConnectUI] = useTonConnectUI();
+  // @ts-ignore
+  const [tonConnectUI, setOptions] = useTonConnectUI();
 
   return (
     <div>
-      <button
-        onClick={() =>
-          tonConnectUI.sendTransaction(transaction as SendTransactionRequest)
-        }
-      >
+      <button onClick={() => tonConnectUI.sendTransaction(myTransaction)}>
         Send transaction
       </button>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react-swc';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/senku-telegram-mini-app/',
+  base: "/senku-telegram-mini-app/",
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
     react(),
+    nodePolyfills(),
     // Allows using the compilerOptions.paths property in tsconfig.json.
     // https://www.npmjs.com/package/vite-tsconfig-paths
     tsconfigPaths(),
@@ -17,11 +19,10 @@ export default defineConfig({
     // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
     // basicSsl(),
   ],
-  publicDir: './public',
+  publicDir: "./public",
   server: {
     // Uncomment this line if you want to expose your dev server and access it from the devices
     // in the same network.
     // host: true,
   },
 });
-
